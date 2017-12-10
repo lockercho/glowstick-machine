d023ffe144eb4b3ab0754fb6effb18bea7e31cfb
Pei-Ya Chiu <lockercho@gmail.com>
2017-12-10 22:12:56 +0800

    Init commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask import request
+import json
+
+app = Flask(__name__, static_url_path='', static_folder='static',)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/_update')
+def _update():
+	message = request.args.get('message', '')
+	socketio.emit('event', message, broadcast=True)
+	return '1'
+
+if __name__ == '__main__':
+    socketio.run(app)
+    print('AAA')
---
+++ static/index.js
@@ -0,0 +1,12 @@
+
+// var state = {
+// 	page: '123',
+// }
+console.log('qwejklqw')
+aler('test')
+window.state = '123lkj'
+
+// var socket = io.connect('http://localhost:5000');
+// socket.on('connect', function() {
+//     socket.emit('message', 'I\'m connected!');
+// });
